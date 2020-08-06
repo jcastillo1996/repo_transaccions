f211496ceb6b69d6fa7fc123600f28c5ea1501d4
jc19961203 <38443191+jc19961203@users.noreply.github.com>
2020-08-06 08:55:58 -0500

    update

--- src/main/java/com/example/demo/controller/TransaccionController.java
+++ src/main/java/com/example/demo/controller/TransaccionController.java
@@ -1,4 +1,5 @@
 package com.example.demo.controller;
+
 import java.time.LocalDateTime;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -24,63 +25,64 @@ import reactor.core.publisher.Mono;
 @RestController
 @RequestMapping("/api_transaccion")
 public class TransaccionController {
-	
+
 	@Autowired
 	TransaccionServiceImpl service;
-	
-	
+
 	@GetMapping
 	public Flux<Transaccion> findAll() {
 
 		return service.findAll();
 	}
-	
+
 	@GetMapping("/test")
 	public Flux<ProductDTO> testtt() {
 
 		return service.findAllProducts();
 	}
+
 	@GetMapping("/test/{id}")
 	public Mono<ProductDTO> testtt1(@PathVariable Long id) {
 
 		return service.findByIdProduct(id);
 	}
+
 	@PostMapping("/test/")
 	public Mono<ProductDTO> te(@RequestBody ProductDTO prod) {
 
 		return service.updateProduct(prod);
 	}
+
 	@GetMapping("/{id}")
 	public Mono<Transaccion> findById(@PathVariable(name = "id") Long id) {
-		return service.findById(id).switchIfEmpty(Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND,"TRANSACCION NOT FOUND ID: "+id)));
+		return service.findById(id).switchIfEmpty(
+				Mono.error(new ResponseStatusException(HttpStatus.NOT_FOUND, "TRANSACCION NOT FOUND ID: " + id)));
 	}
 
 	@PostMapping
 	@ResponseStatus(HttpStatus.CREATED)
-	public Mono<?> save(@RequestBody  Transaccion transaccion) {
-		
-		service.findByIdProduct(transaccion.getProductId())
-		.flatMap(prod->{
-			String prodType=prod.getProductType().getTypeName();
+	public Mono<?> save(@RequestBody Transaccion transaccion) {
+		transaccion.setTransaccionDate(LocalDateTime.now());
+		return service.findByIdProduct(transaccion.getProductId()).flatMap(prod -> {
+			String prodType = prod.getProductType().getTypeName();
 			if (prodType.equals("CUENTA")) {
-				String typeTransac=transaccion.getTransaccionType().getNameType();
+				String typeTransac = transaccion.getTransaccionType().getNameType();
 				if (typeTransac.equals("DEPOSITO")) {
-					
-				}else if (typeTransac.equals("RETIRO")) {
-					
+
+				} else if (typeTransac.equals("RETIRO")) {
+
 				}
-			}else if (prodType.equals("CREDITO")) {
-				
+			} else if (prodType.equals("CREDITO")) {
+
 			}
 			return Mono.just(prod);
 		});
-		transaccion.setTransaccionDate(LocalDateTime.now());
-		return service.save(transaccion);
+
 	}
 
 	@PutMapping
 	public Mono<Transaccion> update(@RequestBody(required = true) Transaccion transaccion) {
-		
+
 		return service.update(transaccion);
 	}
 
